fa0b022ae1005cb244ae27d16cca15dd50e50524
fredricknyangau <nyangaufredrick443@gmail.com>
2025-11-24 10:45:07 +0300

    modified:   frontend/src/services/apiService.ts

--- frontend/src/services/apiService.ts
+++ frontend/src/services/apiService.ts
@@ -3,10 +3,13 @@ export default async (file: File): Promise<Blob> => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch("http://localhost:8000/remove-background", {
-    method: "POST",
-    body: formData,
-  });
+  const response = await fetch(
+    "https://image-background-removal-j7ga.onrender.com/remove-background",
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Failed to process image");
